1538961c788fbf69ee4d6989c88e6e20288f4a05
warrencook6 <warren.cook6@gmail.com>
2017-07-27 11:46:54 -0500

    A few changes to org profile html/ctrl. Just to test something.

--- client/js/controllers/OrganizationProfileController.js
+++ client/js/controllers/OrganizationProfileController.js
@@ -2,11 +2,11 @@ app.controller('OrganizationProfileController',
     function ($scope, userService) {
         console.log('Welcome to an orgs profile!')
 
-
         var jumboHeight = $('.jumbotron').outerHeight();
+
         function parallax() {
             var scrolled = $(window).scrollTop();
-            $('.bg').css('height', (jumboHeight - scrolled) + 'px');
+            $('.orgBG').css('height', (jumboHeight - scrolled) + 'px');
         }
 
         $(window).scroll(function (e) {
