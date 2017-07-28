59526a7e8e14125774c157f9c84b150c5d90050f
warrencook6 <warren.cook6@gmail.com>
2017-07-28 15:20:08 -0500

    peni

--- client/js/controllers/OrganizationProfileController.js
+++ client/js/controllers/OrganizationProfileController.js
@@ -2,25 +2,6 @@ app.controller('OrganizationProfileController',
     function ($scope, userService) {
         console.log('Welcome to an orgs profile!')
 
-$scope.organization = organizaionFactory.get({id: $routeParams.id})
-
-console.log($scope.organization);
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-//jquery dom minuplation
 
         var jumboHeight = $('.jumbotron').outerHeight();
 
@@ -28,7 +9,6 @@ console.log($scope.organization);
             var scrolled = $(window).scrollTop();
             $('.orgBG').css('height', (jumboHeight - scrolled) + 'px');
         }
-
         $(window).scroll(function (e) {
             parallax();
         });
