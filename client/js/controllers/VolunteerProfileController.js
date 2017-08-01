9e484eaf0bf7cd615cf64ab465ef6aa51de2b7ef
warrencook6 <warren.cook6@gmail.com>
2017-08-01 13:53:24 -0500

    not much done

--- client/js/controllers/VolunteerProfileController.js
+++ client/js/controllers/VolunteerProfileController.js
@@ -1,15 +1,12 @@
 app.controller('VolunteerProfileController', ['$scope', 'volunteerFactory', '$routeParams', 'volEventListFactory',
     function($scope, volunteerFactory, $routeParams, volEventListFactory) {
         console.log('Vol Profile Loaded');
-
-        // volunteer facotry 
-        //events factory
-
+        
+             //this returns the users info
         $scope.volunteer = volunteerFactory.get({id: $routeParams.id})
         console.log($scope.volunteer);
 
+            // this returns the events that the user is signed up for
         $scope.volEvents = volEventListFactory.query({id: $routeParams.id});
-        // console.log(volEvents);
-        // $scope.volEvents = volEvents;
         console.log($scope.volEvents);
     }]);
