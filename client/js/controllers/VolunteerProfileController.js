c7a691dde22d09a247ffff1c717ed087cd165a79
warrencook6 <warren.cook6@gmail.com>
2017-08-02 09:16:54 -0500

    attempting to get new posts to go through

--- client/js/controllers/VolunteerProfileController.js
+++ client/js/controllers/VolunteerProfileController.js
@@ -1,12 +1,28 @@
 app.controller('VolunteerProfileController', ['$scope', 'volunteerFactory', '$routeParams', 'volEventListFactory',
-    function($scope, volunteerFactory, $routeParams, volEventListFactory) {
+    function ($scope, volunteerFactory, $routeParams, volEventListFactory) {
         console.log('Vol Profile Loaded');
-        
-             //this returns the users info
-        $scope.volunteer = volunteerFactory.get({id: $routeParams.id})
+
+        //this returns the users info
+        $scope.volunteer = volunteerFactory.get({ id: $routeParams.id })
         console.log($scope.volunteer);
 
-            // this returns the events that the user is signed up for
-        $scope.volEvents = volEventListFactory.query({id: $routeParams.id});
+        // this returns the events that the user is signed up for
+        $scope.volEvents = volEventListFactory.query({ id: $routeParams.id });
         console.log($scope.volEvents);
+
+        var acc = document.getElementsByClassName("accordion");
+        var i;
+
+        // for the accordian
+        for (i = 0; i < acc.length; i++) {
+            acc[i].onclick = function () {
+                this.classList.toggle("active");
+                var panel = this.nextElementSibling;
+                if (panel.style.maxHeight) {
+                    panel.style.maxHeight = null;
+                } else {
+                    panel.style.maxHeight = panel.scrollHeight + "px";
+                }
+            }
+        }
     }]);
